9e1c9812cb9a7e9ace746162afc80aab6667d5ec
Jefferson Lam <jefferson.lam@polymtl.ca>
2024-01-24 21:30:58 -0800

    Quartz sync: Jan 24, 2024, 9:30 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Jefferson Lam",
+    pageTitle: "jlam",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
